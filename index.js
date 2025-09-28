7fd4e0b6d7b8a8a42f4d3c84e0a1290a23fb38ac
aminzo <siyatmohammedcarab@gmail.com>
2025-09-28 21:35:58 +0300

    i did some changes

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code in this file!
+let  currentUser = "abdi"
+let  welcomeMessage = "Welcome to Flatbook, " + 'abdi' + '!'
+
+let  excitedWelcomeMessage =  "WELCOME TO FLATBOOK, " + 'ABDI' + '!'
+
+let   shortGreeting =   "Welcome, " + "a!" 
